aeccf0adfd4749e75c01abe688dc93e7cd1a1550
Vaipik <n.khylko@gmail.com>
2023-06-07 19:31:50 +0300

    frontend mvp

--- services/frontend/src/components/ImageForm.vue
+++ services/frontend/src/components/ImageForm.vue
@@ -1,40 +1,75 @@
 <template>
   <div>
-    <form @submit.prevent>
+    <form @submit.prevent="sendImage">
       <h4>Recognize</h4>
-      <input
-          type="file"
-      >
-      <input
-          type="text"
-          placeholder="Description"
-          v-model="image.label"
-      >
-      <button @click="addPost">Publicate</button>
+      <image-upload v-model="image.img"/>
+      <button type="submit">Recognize</button>
     </form>
   </div>
 </template>
 
 <script>
+import ImageUpload from "@/components/imageUpload.vue";
+import axios from "axios";
+
 export default {
+  components: {ImageUpload},
   data() {
     return {
       image: {
-        img: "",
-        label: "",
-        probability: "",
+        img: null,
+        label: null,
+        probability: null,
       }
     }
   },
   methods: {
-    addPost() {
-      this.post.id = Date.now();
-      this.$emit("create", this.post);
-      this.post = {
-        title: "",
-        body: "",
+    async sendImage() {
+      try {
+        const response = await axios.post(
+            "http://localhost:7001/images/",
+            {
+              file: this.image.img
+            },
+            {
+              headers: {
+                "Content-Type": "multipart/form-data"
+              }
+            }
+        );
+        const reader = new FileReader();
+        reader.readAsDataURL(this.image.img);
+        reader.onload = () => {
+          this.image.id = Date.now()
+          this.image.label = response.data.imageClass;
+          this.image.img = reader.result;
+          console.log(this.image)
+          this.$emit("create", this.image);
+          this.image = {
+            img: null,
+            label: null,
+            probability: null,
+          };
+        };
+
+        console.log("After all", this.image);
+      } catch (e) {
+        console.log(e)
       }
-  }
+
+
+      // const reader = new FileReader();
+      // reader.readAsDataURL(this.image.img);
+      // reader.onload = () => {
+      //   this.image.img = reader.result;
+
+      // this.$emit("create", this.image);
+      // this.image = {
+      //   img: null,
+      //   probability: null,
+      //   label: null,
+      // }
+    }
 
   }
 }
